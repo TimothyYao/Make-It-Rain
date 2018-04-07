3e9bea700899a1c24f6fa660abe72af77b39d2f8
Tim Yao <timothy.liyu.yao@gmail.com>
2018-04-07 17:17:00 -0400

    added links

--- client/modules/App/components/Footer/Footer.js
+++ client/modules/App/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 // Import Style
 import styles from './Footer.css';
@@ -10,6 +11,7 @@ export function Footer() {
   return (
     <div style={{ background: `#FFF url(${bg}) center` }} className={styles.footer}>
       <p>&copy; 2018 &middot; Bitcamp &middot; Arkham City Inc.</p>
+      <p><Link to="/contact" >Contact</Link> &middot; <Link to="/about" >About</Link></p>
     </div>
   );
 }
