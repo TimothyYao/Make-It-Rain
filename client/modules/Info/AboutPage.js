b528d95154db45997ec05eccc052a54fb03382d4
Kirtana Pathak <kirtanapathak@yahoo.com>
2018-04-08 05:59:27 -0400

    text

--- client/modules/Info/AboutPage.js
+++ client/modules/Info/AboutPage.js
@@ -7,10 +7,13 @@ export function AboutPage() {
     <div className={styles.about}>
       <h1>About</h1>
       <hr></hr>
-      <p>Have you ever wanted to make it rain literally and figuritively?
-        Make it Rain is the app for you! Find the location of the person you want
-        to send it to and make it rain (HWA HWA)
-      </p>
+      <p> Our inspiration began in California where citizens are plagued by rough periods of drought and fire.  As the period of drought increases in California, 
+        so does the presence of forest fires. These weather conditions greatly impact the population and the environment of California. </p>
+      <p> Our solution? MAKE IT RAIN! </p>
+      <p>Have you ever wanted to make it rain literally and figuratively?
+        Make it Rain's simple platform allows individuals to make it rain
+        without paper. Simply find the location of the person
+        you want to send money to and swipe! </p>
     </div>
   );
 }
