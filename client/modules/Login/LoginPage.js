b8a38c236ddbb1328f275d09e6c3e706dd8cf671
Tim Yao <timothy.liyu.yao@gmail.com>
2018-04-07 22:13:33 -0400

    login static

--- client/modules/Login/LoginPage.js
+++ client/modules/Login/LoginPage.js
@@ -1,9 +1,31 @@
 /* eslint-disable global-require */
 import React from 'react';
+import styles from './LoginPage.css';
+
 
 export function LoginPage() {
   return (
-    <h1>Login Page Here</h1>
+    <div className={styles.container}>
+      <h1 className={styles.header}>Login</h1>
+      <form>
+        <div className={styles.pic}></div>
+        <div>
+          <label>
+            Username:
+            <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password:
+            <input type="password" name="password" />
+          </label>
+        </div>
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>
   );
 }
 
